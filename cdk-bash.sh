c70b0e679cb309cd7202e531bc8a384ae5584361
Matt Rapczynski <matt@cloudmation.io>
2021-04-20 17:42:36 -0700

    Refactored aliases into shell functions

--- cdk-bash.sh
+++ cdk-bash.sh
@@ -1,7 +1,46 @@
-# Docker CDK Bash Env Setup
+# Docker CDK environment configuration for Bash/Zsh
+
 # Add to .profile or .bashrc Example:
 #  . ~/docker-pycdk/cdk-bash.sh
-alias aws='docker run --rm -it -v ~/.aws:/root/.aws -v $(pwd):/aws amazon/aws-cli' 
-alias pycdk='docker run --rm -it -v ~/.aws:/root/.aws -v $(pwd):/proj cumulusmike/pycdk:active' 
-alias cdk='docker run --rm -it -v ~/.aws:/root/.aws -v $(pwd):/proj cumulusmike/pycdk:active cdk' 
-alias cdkpip='docker run --rm -it -v ~/.aws:/root/.aws -v $(pwd):/proj cumulusmike/pycdk:active pip' 
+
+function aws {
+    docker run --rm -it -v ~/.aws:/root/.aws -v $(pwd):/aws amazon/aws-cli $*
+}
+
+function cdk {
+    docker run --rm -it -v ~/.aws:/root/.aws -v $(pwd):/proj docker.pkg.github.com/cloudmation-llc/docker-pycdk/pycdk:active cdk $*
+}
+
+function pycdk {
+    # Evaluate command
+    if [ "$1" = "add-packages" ]; then
+        pycdk_add_packages ${@:2}
+    elif [ "$1" = "version" ]; then
+        pycdk_set_version $2
+    else
+       echo "pycdk: error: $1 is not a known command"
+    fi
+}
+
+function pycdk_add_packages {
+    echo "pycdk: Installing package(s) $*"
+    docker run --rm -it \
+        -v ~/.aws:/root/.aws \
+        -v $(pwd):/proj \
+        docker.pkg.github.com/cloudmation-llc/docker-pycdk/pycdk:active \
+        pip install --target .pycdk-local $*
+}
+
+function pycdk_set_version {
+    echo "Setting active version to $1"
+
+    # Pull the image from the repository if it does not exist locally
+    docker inspect --format='{{.Id}}' docker.pkg.github.com/cloudmation-llc/docker-pycdk/pycdk:$1
+    if [ $? -eq 1 ]; then
+        echo "pycdk: Fetching image for version $1"
+        docker pull docker.pkg.github.com/cloudmation-llc/docker-pycdk/pycdk:$1
+    fi
+
+    # Apply the 'active' tag to the selected image
+    docker tag docker.pkg.github.com/cloudmation-llc/docker-pycdk/pycdk:$1 docker.pkg.github.com/cloudmation-llc/docker-pycdk/pycdk:active
+}
